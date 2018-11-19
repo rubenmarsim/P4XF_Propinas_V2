957a901b84e53d99c1b61c3f49b853eaf0ee217f
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-19 16:25:33 +0100

    creacion proyecto

---
+++ Propinas/Propinas/Propinas/MainView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Propinas
+{
+	public class MainView : ContentPage
+	{
+		public MainView ()
+		{
+            var lblHeader = new Label
+            {
+                Text = "Propinas Xamarin.Forms Ruben Martinez",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+            var stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    lblHeader
+                }
+            };
+			Content = new ScrollView {
+				Content = stackLayout
+			};
+		}
+	}
+}
