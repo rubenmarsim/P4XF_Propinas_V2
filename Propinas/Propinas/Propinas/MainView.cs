07405e29338ca4f6c8e1504c579b89ea84b46123
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-19 20:06:27 +0100

    add tablas

--- Propinas/Propinas/Propinas/MainView.cs
+++ Propinas/Propinas/Propinas/MainView.cs
@@ -17,40 +17,48 @@ namespace Propinas
                 Text = "Propinas Xamarin.Forms Ruben Martinez",
                 FontSize = 20,
                 FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.CenterAndExpand
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalTextAlignment = TextAlignment.Center
+
             };
             var lblSubtotal = new Label
             {
                 Text = "Subtotal:",
                 FontSize = 10,
                 FontAttributes = FontAttributes.None,
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalTextAlignment = TextAlignment.Center
             };
             var edtrSubtotal = new Editor
             {
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.Center
             };
             var lblPostTax = new Label
             {
                 Text = "Total (Post-Tax):",
                 FontSize = 10,
                 FontAttributes = FontAttributes.None,
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalTextAlignment = TextAlignment.Center
             };
             var edtrPostTax = new Editor
             {
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.Center
             };
             var lblTipPercent = new Label
             {
                 Text = "Tip Percent:",
                 FontSize = 10,
                 FontAttributes = FontAttributes.None,
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalTextAlignment = TextAlignment.Center
             };
             var edtrTipPercent = new Editor
             {
-                HorizontalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.Center,
                 IsEnabled = false,
             };
             var sldr = new Slider
@@ -63,11 +71,13 @@ namespace Propinas
                 Text = "Tip Value:",
                 FontSize = 10,
                 FontAttributes = FontAttributes.None,
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalTextAlignment = TextAlignment.Center
             };
             var edtrTipValue = new Editor
             {
-                HorizontalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.Center,
                 IsEnabled = false,
             };
             var lblTotal = new Label
@@ -75,30 +85,62 @@ namespace Propinas
                 Text = "Total:",
                 FontSize = 10,
                 FontAttributes = FontAttributes.None,
-                HorizontalOptions = LayoutOptions.Fill
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalTextAlignment = TextAlignment.Center
             };
             var edtrTotal = new Editor
             {
-                HorizontalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.Center,
                 IsEnabled = false,               
             };
             var btnCalcular = new Button
             {
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 Text = "Calculate",
+                FontSize = 10,
             };
 
-            var stackLayout = new StackLayout
+            var grid = new Grid
             {
-                Children =
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                RowDefinitions =
                 {
-                    lblHeader, lblSubtotal, edtrSubtotal, lblPostTax, edtrPostTax,
-                    lblTipPercent, edtrTipPercent, lblTipValue, edtrTipPercent,
-                    sldr, lblTipValue, edtrTipValue, lblTotal, edtrTotal, btnCalcular
-                }
+                    new RowDefinition{Height = GridLength.Auto},
+                    new RowDefinition{Height=GridLength.Auto},
+                    new RowDefinition{Height = new GridLength(1, GridUnitType.Auto)},
+                    new RowDefinition{Height=new GridLength(100, GridUnitType.Absolute)}
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width=GridLength.Auto },
+                    new ColumnDefinition{Width=new GridLength(2,GridUnitType.Auto)},
+                    new ColumnDefinition{Width=new GridLength(100,GridUnitType.Absolute)}
+                },
             };
-			Content = new ScrollView {
-				Content = stackLayout
+            grid.Children.Add(lblHeader,1,0);
+            grid.Children.Add(btnCalcular, 2, 0);
+            grid.Children.Add(lblSubtotal,1,1);
+            grid.Children.Add(edtrSubtotal, 2,1);
+            grid.Children.Add(lblPostTax, 1, 2);
+            grid.Children.Add(edtrPostTax, 2, 2);
+            grid.Children.Add(lblTipPercent, 1, 3);
+            grid.Children.Add(edtrTipPercent, 2, 3);
+            grid.Children.Add(sldr,1, 4);
+            grid.Children.Add(lblTipValue, 1, 5);
+            grid.Children.Add(edtrTipValue, 2, 5);
+            grid.Children.Add(lblTotal, 1, 6);
+            grid.Children.Add(edtrTotal, 2, 6);
+
+            //Children =
+            //{
+            //    lblHeader, lblSubtotal, edtrSubtotal, lblPostTax, edtrPostTax,
+            //    lblTipPercent, edtrTipPercent, lblTipValue, edtrTipPercent,
+            //    sldr, lblTipValue, edtrTipValue, lblTotal, edtrTotal, btnCalcular
+            //}
+
+            Content = new ScrollView {
+				Content = grid
 			};
             #endregion
 
