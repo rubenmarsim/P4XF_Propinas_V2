3fa565cba9bfd336e33b0f9b20b3d41b5f6cfecb
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-19 18:03:14 +0100

    funcionamiento basico

--- Propinas/Propinas/Propinas/MainView.cs
+++ Propinas/Propinas/Propinas/MainView.cs
@@ -11,6 +11,7 @@ namespace Propinas
 	{
 		public MainView ()
 		{
+            #region Layout
             var lblHeader = new Label
             {
                 Text = "Propinas Xamarin.Forms Ruben Martinez",
@@ -18,16 +19,103 @@ namespace Propinas
                 FontAttributes = FontAttributes.Bold,
                 HorizontalOptions = LayoutOptions.CenterAndExpand
             };
+            var lblSubtotal = new Label
+            {
+                Text = "Subtotal:",
+                FontSize = 10,
+                FontAttributes = FontAttributes.None,
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var edtrSubtotal = new Editor
+            {
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var lblPostTax = new Label
+            {
+                Text = "Total (Post-Tax):",
+                FontSize = 10,
+                FontAttributes = FontAttributes.None,
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var edtrPostTax = new Editor
+            {
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var lblTipPercent = new Label
+            {
+                Text = "Tip Percent:",
+                FontSize = 10,
+                FontAttributes = FontAttributes.None,
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var edtrTipPercent = new Editor
+            {
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var sldr = new Slider
+            {
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Minimum = 0,
+                Maximum = 100,
+            };
+            var lblTipValue = new Label
+            {
+                Text = "Tip Value:",
+                FontSize = 10,
+                FontAttributes = FontAttributes.None,
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var edtrTipValue = new Editor
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                IsEnabled = false,
+            };
+            var lblTotal = new Label
+            {
+                Text = "Total:",
+                FontSize = 10,
+                FontAttributes = FontAttributes.None,
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            var edtrTotal = new Editor
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                IsEnabled = false,               
+            };
+            var btnCalcular = new Button
+            {
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Text = "Calculate",
+            };
+
             var stackLayout = new StackLayout
             {
                 Children =
                 {
-                    lblHeader
+                    lblHeader, lblSubtotal, edtrSubtotal, lblPostTax, edtrPostTax,
+                    lblTipPercent, edtrTipPercent, lblTipValue, edtrTipPercent,
+                    sldr, lblTipValue, edtrTipValue, lblTotal, edtrTotal, btnCalcular
                 }
             };
 			Content = new ScrollView {
 				Content = stackLayout
 			};
-		}
-	}
+            #endregion
+
+            #region Instancias
+            var CTipInfo = new TipInfo();
+            #endregion
+
+            btnCalcular.Clicked += (sender, e) =>
+            {
+                CTipInfo.Subtotal = double.Parse(edtrSubtotal.Text);
+                CTipInfo.PostTax = double.Parse(edtrPostTax.Text);
+                CTipInfo.TipPercent = double.Parse(edtrTipPercent.Text);
+                CTipInfo.Calculos();
+                edtrTipValue.Text = CTipInfo.TipValue.ToString();
+                edtrTotal.Text = CTipInfo.Total.ToString();
+            };
+
+        }
+    }
 }
---
+++ Propinas/Propinas/Propinas/TipInfo.cs
@@ -0,0 +1,58 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Propinas
+{
+    public class TipInfo
+    {
+        private double _Subtotal;
+
+        public double Subtotal
+        {
+            get { return _Subtotal; }
+            set { _Subtotal = value; }
+        }
+
+        private double _PostTax;
+
+        public double PostTax
+        {
+            get { return _PostTax; }
+            set { _PostTax = value; }
+        }
+
+        private double _TipPercent;
+
+        public double TipPercent
+        {
+            get { return _TipPercent; }
+            set { _TipPercent = value; }
+        }
+
+        private double _TipValue;
+
+        public double TipValue
+        {
+            get { return _TipValue; }
+            set { _TipValue = value; }
+        }
+
+        private double _Total;
+
+        public double Total
+        {
+            get { return _Total; }
+            set { _Total = value; }
+        }
+
+        public void Calculos()
+        {
+            TipValue = PostTax * TipPercent / 100;
+            Total = TipValue + PostTax;
+        }
+
+    }
+}
